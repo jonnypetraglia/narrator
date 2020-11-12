7b8e6263a44bd28f991f8e8bd67fa2f36a9f93b1
Jonny Petraglia <jpetraglia@inspirato.com>
2020-11-11 17:23:59 -0700

    Added Gruntfile for Pumbaa.

---
+++ gruntfile.js
@@ -0,0 +1,3 @@
+// Pumbaa is an automation suite written using Grunt for assisting development at Inspirato.
+// It is not required to develop or install Narrator.
+module.exports = require('../pumbaa/gruntfile');
